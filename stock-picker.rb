c7060542a307e838a51614ab714e9b982c751b64
Reynard Mark Olan <reynardolan@gmail.com>
2022-01-14 06:18:23 +0800

    finalize stock picker method

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,9 +1,11 @@
 def get_input ()
-  puts "Input the amount of days to track max profit"
+  puts "This program calculates for the maximum profit or the minimum loss when selling a stock."
+  puts ""
+  puts "Input the amount of days to track maximum profit or track minimum loss."
   days = gets.chomp.to_i
   price_per_day = []
 
-  puts "Input the price($) per day, press enter every price input"
+  puts "Input the price($) per day, press enter every price input."
   days.times do 
     price_per_day.push(gets.chomp.to_i)
   end
@@ -18,7 +20,11 @@ def stock_picker(days_array)
   profit_per_price_pair = price_pairs_array.map {|price_pair| price_pair[0] - price_pair[1]}
   i = profit_per_price_pair.find_index(profit_per_price_pair.max)
   
-  puts "A profit of $#{price_pairs_array[i][0]} - $#{price_pairs_array[i][1]} == $#{profit_per_price_pair.max}" 
+  if (profit_per_price_pair.max.negative?)
+    puts "A loss of $#{price_pairs_array[i][0]} - $#{price_pairs_array[i][1]} == -$#{profit_per_price_pair.max.abs}"
+  else
+    puts "A profit of $#{price_pairs_array[i][0]} - $#{price_pairs_array[i][1]} == $#{profit_per_price_pair.max}" 
+  end
 end
 
 get_input()
